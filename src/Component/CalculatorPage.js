32ad036f4969c6ed0abd9187df5f73e929344021
Hassan Qadir <77539962+hassan-qadir@users.noreply.github.com>
2023-04-17 04:05:46 +0500

    Update CalculatorPage.js

--- src/Component/CalculatorPage.js
+++ src/Component/CalculatorPage.js
@@ -28,7 +28,7 @@ const CalculatorPage = () => {
     </div>
       <div className='row justify-content-center'>
        <div className='col-md-4'>
-        <div className='card border-primary mb-4'>
+        <div className='card border-primary mb-3'>
         <div className='card-body text-primary'>
            <input type='text' className='form-control form-control-lg shadow' value={input}/>
            <div className='row'>
